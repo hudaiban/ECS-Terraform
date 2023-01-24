ef880af2828a7ae46b9a5b50ab269b4cf7196a41
hudaiban <hudaiban01@hotmail.com>
2023-01-24 17:10:54 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Creating an automate Instance with alibaba cloud using terraform
+
+## prerequisite
+
+<ul>
+<li>Alibaba Cloud Account</li>
+<li>Terraform</li>
+<li>Access Key and secret key [picture will be down below]</li>
+</ul>
+<!-- [photo]: /url/to/img.jpg "Optional Title" -->
+
+# Getting Started
+
+### Getting Secret and Access Key
+
+<ul>
+<li>Login in to your Alibaba Cloud Account</li>
+<li>Go To menu (upper right corner)</li>
+<li>Navigate to [Resource Access Management]</li>
+<li>Click to the user which will appear in users table, eu_admin@...etc</li>
+<li>Create a AccessKy and save the keys (You can not copy secret key after closing pop-up tab)</li>
+</ul>
+
+#### After Following the Code
+
+**Needed Terraform Commands**
+
+<ul>
+
+<li>Terraform init [To start using Terraform with alicloud]</li>
+<li>Terraform plan [To see your configurations]</li>
+<li>Terraform apply [To apply your configurations]</li>
+<li>Terraform destroy [To destroy your ECS]</li>
+
+</ul>
+
+## resources
+
+[link] (https://registry.terraform.io/providers/aliyun/alicloud/latest/docs "alicloud terraform docs")
+[link] (https://www.alibabacloud.com/help/en/basics-for-beginners/latest/regions-and-zones "Region and Zones")
---
+++ createECS.tf
@@ -0,0 +1,60 @@
+
+
+
+#  choose your service provider in this case is Alibaba
+provider "alicloud" {
+  # Access and secret key is a must, it can be found within your account (check README on how to find it)  
+  access_key = "LTAI5tChogEhwaV5rAfUzGe6"
+  secret_key = "x1Y8LZXkUGuPr1GXMClsambrY4BHnv"
+
+  #   Choose a region (this is for sydney)
+  region = "ap-southeast-2"
+}
+
+# Creating instance Type 
+
+data "alicloud_instance_types" "instanceType" {
+  # Choosing a cpu and memory count in which is desired 
+  cpu_core_count = 8
+  memory_size    = 16
+}
+
+
+
+# Create an Image 
+
+data "alicloud_images" "ubuntu" {
+  most_recent = true
+  name_regex  = "^ubuntu_18.*64"
+  owners      = "system"
+}
+
+# Creating an ECS
+
+resource "alicloud_instance" "ubuntu-Terrafrom" {
+  # resoucrs  "calling alicloud instance" | any name you can choose (ops)
+
+  # image ID
+  image_id = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  # Charging type (pay as you go)
+  internet_charge_type = "PayByBandwidth"
+  instance_type        = data.alicloud_instance_types.instanceType.instance_types.0.id
+  #(line above is calling all the lines within bracket in line 29 to 33 above)
+  vswitch_id      = "vsw-p0wj291l5dqk4hrrav75y"
+  security_groups = ["${alicloud_security_group.secGroup.id}"]
+
+
+
+
+}
+
+# Security Group in which is must to have when creating an ECS
+
+
+resource "alicloud_security_group" "secGroup" {
+  name        = "default"
+  description = "default"
+  vpc_id      = "vpc-p0w9vposytkswvwgiucy0"
+}
+
+
